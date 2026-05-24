242d47cde5319f76b46d2b775e071cc03be94f9f
jtmcginley123 <70308424+jtmcginley123@users.noreply.github.com>
2026-05-24 17:54:07 -0500

    added files via upload

---
+++ Assignment 1/Assignment1.sql
@@ -0,0 +1,33 @@
+--Return the first 7 records from the housing table
+
+
+--How many records in the data had sales greater than 100 and a median sales price greater than 100000? 
+--Write a query to return the result.
+
+
+--Show the number of records in the data that had sales greater than 100 or a median sales price less than 100000 by month. 
+--The result should contain two columns: month and num_records.
+
+--Return a result that shows the mean annual sales by city and year, excluding cities that begin with the letter H. 
+--The result should be in descending order by mean sales, limited to 20 records, 
+--and should contain three columns: city, year, and mean_annual_sales
+
+
+--How many sales were there in Austin in December 2010? 
+--Return the city, year, and sales in the result.
+
+--In 2010 in Austin, which month had the most sales? 
+--Return a result that shows the number of sales in Austin in 2010 by month.
+
+--In which city-month combinations did the greatest numbers of sales, outside of Austin?
+--Return a result that contains the top five cities’ sales and the month it took place in 2010, excluding Austin.
+
+
+--In which city, year, and month did the fewest sales take place? 
+--The result should return four columns: fewest_sales, city, year, and month
+
+
+--For the entire dataset, what were the mean sales in each year?
+
+
+--Return only the years that had mean sales greater than 600. Which years does this include?
